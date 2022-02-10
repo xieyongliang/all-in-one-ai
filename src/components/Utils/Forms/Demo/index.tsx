77d73bc588394b582c5e2eb2aeab6e9c1ea9f0ac
Xie <yonglxie@f018984ead2d.ant.amazon.com>
2022-02-10 10:32:46 +0800

    improve demo UI

--- src/components/Utils/Forms/Demo/index.tsx
+++ src/components/Utils/Forms/Demo/index.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, ChangeEvent, useState } from 'react';
 import RadioButton from 'aws-northstar/components/RadioButton';
 import RadioGroup from 'aws-northstar/components/RadioGroup';
-import { Stack, Heading } from 'aws-northstar';
+import { Stack, Heading, Container } from 'aws-northstar';
 import InferenceForm from '../Inference';
 import TransformForm from '../Transform';
 import SampleForm from '../Sample';
@@ -20,35 +20,44 @@ const DemoForm: FunctionComponent<DemoProps> = (props) => {
     if(stateType === '1')
         return (
                 <Stack>
-                    <Heading variant='h4'>{props.name}</Heading>
-                    <RadioGroup onChange={onChange}
-                        items={[
-                            <RadioButton value='0' checked={false}>Batch transform</RadioButton>, 
-                            <RadioButton value='1' checked={true}>Realtime inference with uploaded image</RadioButton>,                
-                            <RadioButton value='2' checked={false}>Realtime inference with sample image</RadioButton>,                
-                        ]}
-                    />
-                    <InferenceForm/>
+                    <Heading variant='h1'>{props.name}</Heading>
+                    <Container title = "Demo type">
+                        <RadioGroup onChange={onChange}
+                            items={[
+                                <RadioButton value='0' checked={false}>Batch transform</RadioButton>, 
+                                <RadioButton value='1' checked={true}>Realtime inference with uploaded image</RadioButton>,                
+                                <RadioButton value='2' checked={false}>Realtime inference with sample image</RadioButton>,                
+                            ]}
+                        />
+                    </Container>
+                    <Container title = "Realtime inference with uploaded image">
+                        <InferenceForm/>
+                    </Container>
                 </Stack>
         )
     else if(stateType === '0')
         return (
             <Stack>
-                <Heading variant='h4'>{props.name}</Heading>
-                <RadioGroup onChange={onChange}
-                    items={[
-                        <RadioButton value='0' checked={true}>Batch transform</RadioButton>, 
-                        <RadioButton value='1' checked={false}>Realtime inference with uploaded image</RadioButton>,                
-                        <RadioButton value='2' checked={false}>Realtime inference with sample image</RadioButton>,                
-                ]}
-                />
-                <TransformForm/>
+                <Heading variant='h1'>{props.name}</Heading>
+                <Container title = "Demo type">
+                    <RadioGroup onChange={onChange}
+                        items={[
+                            <RadioButton value='0' checked={true}>Batch transform</RadioButton>, 
+                            <RadioButton value='1' checked={false}>Realtime inference with uploaded image</RadioButton>,                
+                            <RadioButton value='2' checked={false}>Realtime inference with sample image</RadioButton>,                
+                    ]}
+                    />
+                </Container>
+                <Container title = "Batch transform">
+                    <TransformForm/>
+                </Container>
             </Stack>
         )
     else
         return (
             <Stack>
-                <Heading variant='h4'>{props.name}</Heading>
+                <Heading variant='h1'>{props.name}</Heading>
+                <Container title = "Demo type">
                 <RadioGroup onChange={onChange}
                     items={[
                         <RadioButton value='0' checked={false}>Batch transform</RadioButton>, 
@@ -56,7 +65,10 @@ const DemoForm: FunctionComponent<DemoProps> = (props) => {
                         <RadioButton value='2' checked={true}>Realtime inference with sample image</RadioButton>,                
                 ]}
                 />
-                <SampleForm/>
+                </Container>
+                <Container title = "Realtime inference with sample image">
+                    <SampleForm/>
+                </Container>
             </Stack>
     )
 }
