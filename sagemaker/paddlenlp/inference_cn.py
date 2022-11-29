310856d0677013358098ff17f9920c5524313734
xieyongliang <yongliangxie@hotmail.com>
2022-11-29 11:44:08 +0800

    revise support for China region

---
+++ sagemaker/paddlenlp/inference_cn.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import json
+import argparse
+import numpy as np
+
+def model_fn(model_dir):
+    """
+    Load the model for inference
+    """
+
+    args = parse_args()
+    args.model_path_prefix = os.path.join(model_dir, 'inference')
+    args.device = os.environ['device'] if('device' in os.environ) else 'cpu'
+    args.schema = json.loads(os.environ['schema'])
+
+    if(args.device == 'gpu'):
+        os.system('pip install -U paddlepaddle-gpu -i https://pypi.tuna.tsinghua.edu.cn/simple')
+        os.system('pip install -U onnxruntime-gpu -i https://pypi.tuna.tsinghua.edu.cn/simple')
+        os.system('pip install nvgpu -i https://pypi.tuna.tsinghua.edu.cn/simple')
+    else:
+        os.system('pip install -U paddlepaddle-i https://pypi.tuna.tsinghua.edu.cn/simple')
+        os.system('pip install -U onnxruntime -i https://pypi.tuna.tsinghua.edu.cn/simple')
+
+    from uie_predictor import UIEPredictor
+
+    predictor = UIEPredictor(args)
+    return predictor
+
+
+def input_fn(request_body, request_content_type):
+    """
+    Deserialize and prepare the prediction input
+    """
+
+    if request_content_type == 'application/json':
+        request = json.loads(request_body)
+    else:
+        # Handle other content-types here or raise an Exception
+        # if the content type is not supported.  
+        request = request_body
+    
+    return request
+
+    
+def predict_fn(input_data, model):
+    """
+    Apply model to the incoming request
+    """
+        
+    inputs = [ input_data['inputs'] ]
+
+    outputs = model.predict(inputs)
+
+    result =  {
+        'result': outputs
+    }
+    
+    return result
+
+def output_fn(prediction, response_content_type):
+    """
+    Serialize and prepare the prediction output
+    """
+
+    if response_content_type == "application/json":
+        response = json.dumps(prediction, ensure_ascii = False, cls = MyEncoder)
+    else:
+        response = str(prediction)
+
+    return response    
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+#     # Required parameters
+#     parser.add_argument(
+#         "--model_path_prefix",
+#         type=str,
+#         required=True,
+#         help="The path prefix of inference model to be used.", )
+    parser.add_argument(
+        "--position_prob",
+        default=0.5,
+        type=float,
+        help="Probability threshold for start/end index probabiliry.", )
+    parser.add_argument(
+        "--max_seq_len",
+        default=512,
+        type=int,
+        help="The maximum input sequence length. Sequences longer than this will be split automatically.",
+    )
+    
+    parsed, unknown = parser.parse_known_args() # this is an 'internal' method
+    # which returns 'parsed', the same as what parse_args() would return
+    # and 'unknown', the remainder of that
+    # the difference to parse_args() is that it does not exit when it finds redundant arguments
+
+    for arg in unknown:
+        if arg.startswith(("-", "--")):
+            # you can pass any arguments to add_argument
+            parser.add_argument(arg, type=str)
+
+    args = parser.parse_args()
+    return args
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+    
---
+++ sagemaker/paddleocr/inference_cn.py
@@ -0,0 +1,133 @@
+import io
+import os
+import json
+import boto3
+import numpy as np
+from PIL import Image
+from numpy import asarray
+s3_client = boto3.client('s3')
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+def model_fn(model_dir):
+    """
+    Load the model for inference
+    """
+
+    task =  os.environ['task'] if('task' in os.environ) else 'ocr'
+    device = os.environ['device'] if('device' in os.environ) else 'cpu'
+    det_model_dir = os.environ['det_model_dir'] if('det_model_dir' in os.environ) else None
+    rec_model_dir = os.environ['rec_model_dir'] if('rec_model_dir' in os.environ) else None
+    cls_model_dir = os.environ['cls_model_dir'] if('cls_model_dir' in os.environ) else None
+    table_model_dir = os.environ['table_model_dir'] if('table_model_dir' in os.environ) else None
+    rec_char_dict_path = os.environ['rec_char_dict_path'] if('rec_char_dict_path' in os.environ) else None
+    table_char_dict_path = os.environ['table_char_dict_path'] if('table_char_dict_path' in os.environ) else None
+    lang = os.environ['lang'] if('lang' in os.environ) else 'ch'
+    table = os.environ['table'] if('table' in os.environ) else True
+    layout = os.environ['layout'] if('layout' in os.environ) else True
+    ocr = os.environ['ocr'] if('ocr' in os.environ) else True
+    use_gpu = (device == 'gpu')
+
+    if(device == 'gpu'):
+        os.system('pip install -U paddlepaddle-gpu -i https://pypi.tuna.tsinghua.edu.cn/simple')
+    else:
+        os.system('pip install -U paddlepaddle -i https://pypi.tuna.tsinghua.edu.cn/simple')
+
+    from paddleocr import PaddleOCR, PPStructure
+    
+    if task == 'ocr':
+        engine = PaddleOCR(
+            use_gpu=use_gpu, 
+            use_angle_cls=True, 
+            lang=lang, 
+            det_model_dir=det_model_dir, 
+            rec_model_dir=rec_model_dir, 
+            cls_model_dir=cls_model_dir
+        )
+    else:
+        engine = PPStructure(
+            use_gpu=use_gpu, 
+            lang=lang, 
+            det_model_dir=det_model_dir, 
+            rec_model_dir=rec_model_dir, 
+            table_model_dir=table_model_dir, 
+            rec_char_dict_path=rec_char_dict_path, 
+            table_char_dict_path=table_char_dict_path,
+            layout=layout,
+            table=table,
+            ocr=ocr
+        )
+
+    print(engine)
+    print(task)
+    return engine, task
+
+
+def input_fn(request_body, request_content_type):
+    """
+    Deserialize and prepare the prediction input
+    """
+    
+    if request_content_type == 'image/jpg' or request_content_type == 'image/jpeg' or request_content_type == 'image/png':
+        bytes = request_body
+        image = Image.open(io.BytesIO(bytes)).convert('RGB')
+        return asarray(image)
+    elif request_content_type == 'application/json':
+        data = json.loads(request_body)
+        bucket = data['bucket']
+        image_uri = data['image_uri']
+        s3_object = s3_client.get_object(Bucket = bucket, Key = image_uri) 
+        bytes = s3_object["Body"].read()
+        image = Image.open(io.BytesIO(bytes)).convert('RGB')
+        return asarray(image)
+    else:
+        return request_body
+    
+def predict_fn(input_data, model):
+    """
+    Apply model to the incoming request
+    """
+    engine, task = model
+    if(task == 'ocr'):
+        preds = engine.ocr(input_data, rec=True)
+    else:
+        preds = engine(input_data)
+
+    if(task == 'ocr'):
+        result = {}
+
+        label = []
+        confidence = []
+        bbox = []
+        for pred in preds:
+            label.append(pred[1][0])
+            confidence.append(pred[1][1])
+            bbox.append(pred[0])
+
+        result['label'] = label
+        result['confidence'] = confidence
+        result['bbox'] = bbox
+    else:
+        result = []
+        for pred in preds:
+            result.append(pred['res'])
+
+    response = json.dumps(result, ensure_ascii = False, cls = MyEncoder)
+
+    return response    
+
+def output_fn(prediction, content_type):
+    """
+    Serialize and prepare the prediction output
+    """
+
+    return prediction
