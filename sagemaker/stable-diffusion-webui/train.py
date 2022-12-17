c588838bc45236b18c7f9fddfd2739576a6fa84e
Ubuntu <ubuntu@ip-172-31-3-103.us-west-1.compute.internal>
2022-12-17 14:32:31 +0000

    add xformers support for sagemaker training and endpoint

--- sagemaker/stable-diffusion-webui/train.py
+++ sagemaker/stable-diffusion-webui/train.py
@@ -14,7 +14,7 @@ parser.add_argument('--api-endpoint', default='', type=str, help='API Endpoint')
 
 args = parser.parse_args()
 
-cmd = "bash webui.sh --port 8080 --listen --train --train-task {0} --train-args '{1}' --embeddings-dir /opt/ml/input/data/embeddings --hypernetwork-dir /opt/ml/input/data/hypernetwork --ckpt {2} --ckpt-dir /opt/ml/input/data/models --region-name {3} --api-endpoint {4}".format(args.train_task, args.train_args, args.ckpt, args.region_name, args.api_endpoint)
+cmd = "bash webui.sh --port 8080 --listen --xformers --train --train-task {0} --train-args '{1}' --embeddings-dir /opt/ml/input/data/embeddings --hypernetwork-dir /opt/ml/input/data/hypernetwork --ckpt {2} --ckpt-dir /opt/ml/input/data/models --region-name {3} --api-endpoint {4}".format(args.train_task, args.train_args, args.ckpt, args.region_name, args.api_endpoint)
 
 if args.embeddings_s3uri != '':
     cmd = '{0} --embeddings-s3uri {1}'.format(cmd, args.embeddings_s3uri)
