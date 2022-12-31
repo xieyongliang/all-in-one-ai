62cd7fd8065af0a1ce191a6532b04d4021e5c9e4
xieyongliang <yongliangxie@hotmail.com>
2022-12-31 20:07:07 +0800

    add support accelerate, deepspeed, triton support

--- sagemaker/stable-diffusion-webui/train.py
+++ sagemaker/stable-diffusion-webui/train.py
@@ -16,7 +16,7 @@ parser.add_argument('--dreambooth-config-id', default='', type=str, help='Dreamb
 
 args = parser.parse_args()
 
-cmd = "LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH bash webui.sh --port 8080 --listen --xformers --train --train-task {0} --train-args '{1}' --embeddings-dir /opt/ml/input/data/embeddings --hypernetwork-dir /opt/ml/input/data/hypernetwork --lora-models-path /opt/ml/input/data/lora --dreambooth-models-path /opt/ml/input/data/dreambooth  --ckpt {2} --ckpt-dir /opt/ml/input/data/models --region-name {3} --api-endpoint {4}".format(args.train_task, args.train_args, args.ckpt, args.region_name, args.api_endpoint)
+cmd = "LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH ACCELERATE=true bash webui.sh --port 8080 --listen --xformers --train --train-task {0} --train-args '{1}' --embeddings-dir /opt/ml/input/data/embeddings --hypernetwork-dir /opt/ml/input/data/hypernetwork --lora-models-path /opt/ml/input/data/lora --dreambooth-models-path /opt/ml/input/data/dreambooth  --ckpt {2} --ckpt-dir /opt/ml/input/data/models --region-name {3} --api-endpoint {4}".format(args.train_task, args.train_args, args.ckpt, args.region_name, args.api_endpoint)
 
 if args.embeddings_s3uri != '':
     cmd = '{0} --embeddings-s3uri {1}'.format(cmd, args.embeddings_s3uri)
