d11b9935411e51fd1033d1000cd99d65117c4d0d
xieyongliang <yongliangxie@hotmail.com>
2023-01-16 09:30:46 +0800

    fix issues with s3 presigned url

--- backend/src/all_in_one_ai_s3/lambda_function.py
+++ backend/src/all_in_one_ai_s3/lambda_function.py
@@ -4,7 +4,7 @@ import traceback
 from botocore.exceptions import ClientError
 from botocore.client import Config
 
-s3_client = boto3.client('s3', config=Config(signature_version='s3v4'))
+s3_client = boto3.client('s3', config=boto3.session.Config(s3={'addressing_style': 'virtual'}, signature_version='s3v4'))
 s3_resource = boto3.resource('s3')
 
 def lambda_handler(event, context):
