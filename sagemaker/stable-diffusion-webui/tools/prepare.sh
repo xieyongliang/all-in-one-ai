b6f9f0a28c5153c69d4de2cecd712c00801044ad
xie river <chuanxie@amazon.com>
2023-04-14 05:33:50 +0000

    remove model.tar.gz

--- sagemaker/stable-diffusion-webui/tools/prepare.sh
+++ sagemaker/stable-diffusion-webui/tools/prepare.sh
@@ -9,12 +9,8 @@ if [ ! -f "./s5cmd" ]; then
     tar xzvf s5cmd_2.0.0_Linux-64bit.tar.gz
 fi
 
-echo "Please make sure that you have already put model.tar.gz into assets directory and SD checkpoints (.ckpt) into models directory, please refer to Prepare SD models at https://docs.ai.examples.pro/stable-diffusion-webui/"
-
 userid=$(aws sts get-caller-identity | jq -r .Account)
 region=$1
 aws s3 mb s3://sagemaker-${region}-${userid} --region ${region}
-echo "Uploading assets/model.tar.gz to s3://sagemaker-${region}-${userid}/stable-diffusion-webui/assets/"
-AWS_REGION=${region} ./s5cmd cp assets/model.tar.gz s3://sagemaker-${region}-${userid}/stable-diffusion-webui/assets/
 echo "Uploading models/ to s3://sagemaker-${region}-${userid}/stable-diffusion-webui/models/"
 AWS_REGION=${region} ./s5cmd cp models/ s3://sagemaker-${region}-${userid}/stable-diffusion-webui/models/
