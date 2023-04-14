c7307e9bf03228c9be04d3a09bcff28bf160d117
xieyongliang <yongliangxie@hotmail.com>
2023-04-14 14:56:29 +0800

    Update run.js

--- web/run.js
+++ web/run.js
@@ -430,16 +430,7 @@ app.use(createProxyMiddleware('/train', {
     changeOrigin: true,
     secure: false,
     ws: false,
-}));    
-app.use(createProxyMiddleware('/process', {
-    target: ApiURL + '/process',
-    pathRewrite: {
-        '^/process': ''
-    },
-    changeOrigin: true,
-    secure: false,
-    ws: false,
-}));  
+}));
 app.use(createProxyMiddleware('/deploy', {
     target: ApiURL + '/deploy',
     pathRewrite: {
